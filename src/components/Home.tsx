6db141dd04050237ee4cff0756d5979ef7f18d62
Karan Tomar <115060517+karantomar-26@users.noreply.github.com>
2024-07-09 15:06:54 +0530

    Update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,7 @@
 
 import Card from './Card'
 import Nav from './Nav'
-import {ProductContext} from '../Utils/Context'
+import {ProductContext,ProductContextType, ProductType} from '../Utils/Context'
 import { useContext, useEffect, useState } from 'react'
 import Loading from './Loading';
 import { useLocation, useParams } from 'react-router-dom';
@@ -9,9 +9,9 @@ import axios from 'axios';
 
 function Home() {
 
-  const {products} = useContext(ProductContext);
+  const {products} = useContext(ProductContext)!;
 
-  const [filteredProducts, setfilteredProducts] = useState(null);
+  const [filteredProducts, setfilteredProducts] = useState<ProductType[] | null>(null);
 
   const { search } = useLocation();
   const category = decodeURIComponent(search.split('=')[1]);
@@ -41,7 +41,7 @@ function Home() {
       <Nav />
       <div className='h-screen overflow-y-auto hide-scrollbar w-[85%] p-5 pt-[4%] pb-9'>
         <div className='flex flex-wrap items-center  '>
-          {filteredProducts && Array.isArray(filteredProducts) && filteredProducts.map((p: ProductContextType, i: number) => (
+          {filteredProducts && Array.isArray(filteredProducts) && filteredProducts.map((p: ProductType, i) => (
             <Card key={i} product={p} />
           ))};
         </div>
