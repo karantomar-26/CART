80a9797dbc042df00c3b0f722d7a8e116f13ec0b
Karan Tomar <115060517+karantomar-26@users.noreply.github.com>
2024-07-09 15:07:46 +0530

    Update Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,8 +1,7 @@
 
-import React from 'react'
 import { Link } from 'react-router-dom'
 
-function Card({ product }: { product: { title: string; image: string } }) {
+function Card({ product }: { product: { title: string; image: string; id: Number } }) {
   return (
     <Link to={`/details/${product.id}`} className="border overflow-hidden rounded-lg p-2 w-[20%] h-[35vh] flex items-center flex-col">
       <div className="hover:scale-110 w-[70%] h-[35vh] bg-contain bg-center bg-no-repeat" style={{
